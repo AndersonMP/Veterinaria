15e17a14c2f7de90a9b4195c600b040acd0bdd3d
AndersonMP <andersonfer20@gmail.com>
2025-12-18 17:27:41 -0500

    Reto 11

---
+++ src/main/java/com/krakedev/veterinaria/repository/MascotaRepository.java
@@ -0,0 +1,15 @@
+package com.krakedev.veterinaria.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.krakedev.veterinaria.entity.Mascota;
+
+@Repository
+public interface MascotaRepository extends JpaRepository<Mascota, Long> {
+    Optional<Mascota> findByMascotaId(Long mascotaId);
+
+    Optional<Mascota> findByMascotaNombre(String nombreMascota);
+}
