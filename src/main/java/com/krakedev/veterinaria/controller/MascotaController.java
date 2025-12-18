9446fb1a8f97163205de285764ffd83524f66043
AndersonMP <andersonfer20@gmail.com>
2025-12-18 13:00:40 -0500

    Reto 5

---
+++ src/main/java/com/krakedev/veterinaria/controller/MascotaController.java
@@ -0,0 +1,29 @@
+package com.krakedev.veterinaria.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.krakedev.veterinaria.entity.Mascota;
+
+@RestController
+@RequestMapping("/api/mascotas")
+public class MascotaController {
+    private List<Mascota> mascotas = new ArrayList<>();
+
+    public MascotaController() {
+        mascotas.add(new Mascota(1, "Firulais", "Perro", 3, "Juan Perez"));
+        mascotas.add(new Mascota(2, "Michi", "Gato", 2, "Ana Gomez"));
+        mascotas.add(new Mascota(3, "Nemo", "Pez", 1, "Carlos Lopez"));
+        mascotas.add(new Mascota(4, "Bubbles", "Pez", 1, "Maria Rodriguez"));
+        mascotas.add(new Mascota(5, "Rex", "Perro", 4, "Luis Martínez"));
+    }
+
+    @GetMapping()
+    public List<Mascota> listarMascotas() {
+        return mascotas;
+    }
+}
