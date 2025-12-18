8d3a2fdc392f00369d51c29aefff53a0ba355e2e
AndersonMP <andersonfer20@gmail.com>
2025-12-18 13:30:15 -0500

    Reto 7

--- src/main/java/com/krakedev/veterinaria/controller/MascotaController.java
+++ src/main/java/com/krakedev/veterinaria/controller/MascotaController.java
@@ -6,9 +6,10 @@ import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.krakedev.veterinaria.entity.Mascota;
 
 @RestController
@@ -36,5 +37,12 @@ public class MascotaController {
                 .findFirst();
 
         return mascota.orElse(null);
+    }
+
+    @PostMapping
+    public Mascota crearMascota(@RequestBody Mascota nuevaMascota) {
+
+        mascotas.add(nuevaMascota);
+        return nuevaMascota;
     }
 }
