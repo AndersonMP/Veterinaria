62180bcde7f1c843ad4baebae68a9f8a715258b8
AndersonMP <andersonfer20@gmail.com>
2025-12-18 20:57:10 -0500

    Reto 14

--- src/main/java/com/krakedev/veterinaria/controller/MascotaController.java
+++ src/main/java/com/krakedev/veterinaria/controller/MascotaController.java
@@ -1,10 +1,12 @@
 package com.krakedev.veterinaria.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -59,6 +61,22 @@ public class MascotaController {
         Mascota nuevaMascota = mascotaService.registrarMascota(mascota);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevaMascota);
+    }
+
+    @GetMapping("/buscar/nombre/{nombre}")
+    public ResponseEntity<?> buscarPorNombre(@PathVariable String nombre) {
+        Optional<Mascota> mascota = mascotaService.buscarPorNombre(nombre);
+        return mascota.isPresent() ? ResponseEntity.ok().body(mascota.get())
+                : ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("Mascota con nombre " + nombre + " no encontrada");
+    }
+
+    @GetMapping("/buscar/id/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
+        Optional<Mascota> mascota = mascotaService.buscarPorId(id);
+        return mascota.isPresent() ? ResponseEntity.ok().body(mascota.get())
+                : ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("Mascota con ID " + id + " no encontrada");
     }
 
     // @DeleteMapping("/{id}")
